5d06f670013afa72513f7385247a4fde50dd5c2b
Ivan Tymchenko <tymchenkolviv@gmail.com>
2021-09-15 03:25:59 +0300

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          Annochka
         </p>
         <a
           className="App-link"
